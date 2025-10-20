71c3c070957a987e91e4925e53ab1f4866de1d37
joshua <joshua.t@outlook.de>
2025-10-20 21:16:42 +0200

    use tuple

--- math_wuerfel.py
+++ math_wuerfel.py
@@ -1,4 +1,6 @@
 import time
+from collections import defaultdict
+
 from tqdm import tqdm
 def get_all_possibilities() -> list:
     possibilities = list()
@@ -17,18 +19,16 @@ def is_win(game_val: tuple):
     return False
 
 def remove_values(arr_1, min_value):
-    return [arr_1[0] - min_value,arr_1[1] - min_value, arr_1[2] - min_value, arr_1[3] - min_value]
-def add_values(arr_1: list, arr_2):
-    return [arr_1[0] + arr_2[0], arr_1[1] + arr_2[1], arr_1[2] + arr_2[2],arr_1[3] + arr_2[3]]
+    return arr_1[0] - min_value,arr_1[1] - min_value, arr_1[2] - min_value, arr_1[3] - min_value
+def add_values(arr_1: tuple, arr_2):
+    return arr_1[0] + arr_2[0], arr_1[1] + arr_2[1], arr_1[2] + arr_2[2],arr_1[3] + arr_2[3]
 
 def run_game(current_game_state: dict, possibilities: list, already_won):
     """Erstellt ein neues state dictonary und printet die wins und games der runde"""
-    game_dict = {}
+    game_dict = defaultdict(int)
     games = already_won
     wins = already_won
-    for key, value in tqdm(current_game_state.items()):
-        #wandelt den dict key zu einem int array um
-        game_state = [int(i) for i in key.split("-")]
+    for game_state, value in tqdm(current_game_state.items()):
         for i in possibilities:
             new_state = add_values(game_state, i)
             #zieht von allem dem minimalen wert ab um das dictonary kleiner zu halten
@@ -36,12 +36,8 @@ def run_game(current_game_state: dict, possibilities: list, already_won):
             new_state = remove_values(new_state, min_value)
             if not is_win(new_state):
                 #sortiert die würfelsummen nach groeße um das dict kleiner zu halten
-                new_state = sorted(new_state)
-                new_state_key = "-".join([str(i) for i in new_state])
-                if new_state_key not in game_dict:
-                    game_dict[new_state_key] = value
-                else:
-                    game_dict[new_state_key] += value
+                new_state = tuple(sorted(new_state))
+                game_dict[new_state] += value
             else:
                 wins += value
             games += value
@@ -59,13 +55,12 @@ def just_count(current_game_state: dict, possibilities, already_won):
             if is_win(add_values(i, game_state)):
                 wins += value
             games += value
-
     print(wins, games, wins/games)
 
 
 def run_games(games_to_simulate: int):
     possibilities = get_all_possibilities()
-    current_game = {"0-0-0-0": 1}
+    current_game = {(0,0,0,0): 1}
     already_won = 0
     for i in range(games_to_simulate - 1):
         start_time = time.time()
